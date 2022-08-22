bd3d8a53e56a3501dd712877e1279d0bd71fd7ae
Karl Rosengren <60710855+Goby56@users.noreply.github.com>
2022-08-22 15:18:46 +0200

    kattis problems

---
+++ kattis/3dprinter.py
@@ -0,0 +1,12 @@
+# https://open.kattis.com/problems/3dprinter
+
+days = 0
+printers = 1
+statues = 0
+goal = 10
+
+while statues < goal:
+    for p in range(printers):
+        pass
+
+    days += 1
---
+++ kattis/aaah.py
@@ -0,0 +1,4 @@
+jon_ah = input()
+doc_ah = input()
+
+print("go") if len(jon_ah) > len(doc_ah) else print("no")
