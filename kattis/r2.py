7847de3ed90bc94ca374bd736776b5aafb05ce3c
Karl Rosengren <60710855+Goby56@users.noreply.github.com>
2022-08-31 11:02:25 +0200

    project euler progress
    ordered fractions

---
+++ kattis/r2.py
@@ -0,0 +1,8 @@
+# https://open.kattis.com/problems/r2
+
+i = input().split()
+r1, s = int(i[0]), int(i[1])
+
+r2 = 2*s - r1
+
+print(r2)
--- project euler/orderedfractions.py
+++ project euler/orderedfractions.py
@@ -1,20 +1,59 @@
 # https://projecteuler.net/problem=71
 
-fractions = []
+N = 1e6
 
 
-def factors(n):
-    # find all factors of n
-    # test 0-9 and all primes up to 10^6
-    pass
+def find_primes_under(n) -> list:
+    primes = []
+
+    for i in range(2, n):
+        if is_prime(i):
+            primes.append(i)
+    return primes
+
+
+def is_prime(n: int) -> bool:
+    for d in range(2, n):
+        if n % d == 0:
+            return False
+    return True
+
+
+PRIMES = find_primes_under(200)
+
+
+def primes_under(n: int) -> list:
+    primes = []
+    for p in PRIMES:
+        if p > n:
+            return primes[::-1]
+        primes.append(p)
+
+
+def factors_of(n: int) -> list:
+    factors = [n] if is_prime(n) else []
+
+    for p in primes_under(n):
+        if n % p == 0:
+            factors.append(p)
+            factors.extend(factors_of(n // p))
+            # factors are bring appended twice
+            break
+    return factors
+
+
+print(factors_of(50))
 
 
 def hcf(n, d):
-    
     pass
 
 
-for d in range(int(1e6)):
-    for n in range(d):
-        if hcf(n, d):
-            fractions.append((n, d))
+def proper_fractions_under(n: int) -> list:
+    proper_fractions = []
+
+    for d in range(n):
+        for n in range(d):
+            if hcf(n, d):
+                proper_fractions.append((n, d))
+    return proper_fractions
