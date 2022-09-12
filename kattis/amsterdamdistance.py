10e13d31264133b9be448a860ac2934b1109a8fe
Goby56 <60710855+Goby56@users.noreply.github.com>
2022-09-12 22:47:05 +0200

    amsterdam distance

--- kattis/amsterdamdistance.py
+++ kattis/amsterdamdistance.py
@@ -1,8 +1,16 @@
+# https://open.kattis.com/problems/amsterdamdistance?editsubmit=9374129
+
 from math import pi
 
 M, N, R = [float(x) for x in input().split()]
 ax, ay, bx, by = [int(x) for x in input().split()]
 
+angle = pi * (abs(ax - bx) / M) # The angle between the two streets
 
-D = R * abs(ay - by) / N + R * min(ay, by) * pi * abs(ax - bx) / M 
-print(D)
+arc_len = R * (min(ay, by) / N) * angle # The length of the shortest arc
+straight_len = 2 * R * min(ay, by) / N # The length of the straight inner path from each point of the shortest arc
+
+diff_len = R * abs(ay - by) / N # The length from canal A to canal B
+
+print(min(arc_len, straight_len) + diff_len)
+print("arc") if arc_len < straight_len else print("straight")
