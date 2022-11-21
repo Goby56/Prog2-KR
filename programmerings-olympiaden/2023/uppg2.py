0ca703d9623199604315f85edf4131dba39fda4d
Goby56 <60710855+Goby56@users.noreply.github.com>
2022-11-21 21:39:20 +0100

    20 edge case fix

--- programmerings-olympiaden/2023/uppg2.py
+++ programmerings-olympiaden/2023/uppg2.py
@@ -4,7 +4,6 @@ letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
 
 solutions = []
 
-
 def decrypted_solutions(krypto: str, sol: list):
     if len(krypto) < 2:
         if len(krypto) == 1:
@@ -16,18 +15,17 @@ def decrypted_solutions(krypto: str, sol: list):
     if two_digit > 29 or two_digit < 10:
         sol.append(letters[int(krypto[0])-1])
         decrypted_solutions(krypto[1:], sol)
-    elif two_digit <= 29 and two_digit > 10:
+    elif two_digit <= 29 and two_digit > 10 and two_digit != 20:
         alt_sol = sol.copy()
         sol.append(letters[int(krypto[0])-1])
         decrypted_solutions(krypto[1:], sol)
-
         alt_sol.append(letters[two_digit-1])
         decrypted_solutions(krypto[2:], alt_sol)
-    elif two_digit == 10:
+    elif two_digit == 10 or two_digit == 20:
         sol.append(letters[int(krypto[:2])-1])
         decrypted_solutions(krypto[2:], sol)
 
 
 decrypted_solutions(encrypted_string, [])
-
+print(solutions)
 print("Svar:", len(solutions))
